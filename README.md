a1ad5d7930be2bdab4b202027f48d92e5313a10f
dhiraj_baruah <baruahd5@gmail.com>
2021-11-07 02:08:53 +0530

    warning added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # emotion-detection
+
+Learn to make an Image Classifier with Tensorflow.js Node.js and Google Teachable Machine
+
+This video implements the concept of client-side artificial neural networks. We will learn how to run models along with full deep learning applications in the browser! We’ll be using TensorFlow.js (TFJS), a TensorFlow’s JavaScript library, Node.js and Google Teachable Machine.
+
+By the end of this video tutorial, you will have built a web application that runs a neural network in the browser to classify images!
+
+Medium Post - https://medium.com/@baruahd5/create-image-classifier-with-node-js-tensorflow-js-google-teachable-machine-280ee5f92bad
+
+Google Teachable Machine - https://teachablemachine.withgoogle.com/
--- static/predict.js
+++ static/predict.js
@@ -1,68 +1,63 @@
- // More API functions here:
-    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+// the link to your model provided by Teachable Machine export panel
+const URL = `tm-my-image-model/`;
 
-    // the link to your model provided by Teachable Machine export panel
-    const URL = `tm-my-image-model/`;
+let model, webcam, maxPredictions, happy, sad, angry;
+let refresh = true;
 
-    let model, webcam, maxPredictions, happy, sad, angry;
-    let refresh = true;
+// Load the image model and setup the webcam
+async function init() {
+    if (refresh) {
+        refresh = false;
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
 
-    // Load the image model and setup the webcam
-    async function init() {
-        if(refresh) {
-            refresh = false;
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-    
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
-    
-            // Convenience function to setup a webcam
-            const flip = true; // whether to flip the webcam
-            webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
-            await webcam.setup(); // request access to the webcam
-            await webcam.play();
-            window.requestAnimationFrame(loop);
-    
-            // append elements to the DOM
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
-            happy = document.getElementById("happy");
-            sad = document.getElementById("sad");
-            angry = document.getElementById("angry");
+        // load the model and metadata
 
-        }
-        else{
-            location.reload();
-        }
-       
-    }
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
 
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
+        // Convenience function to setup a webcam
+        const flip = true; // whether to flip the webcam
+        webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        // append elements to the DOM
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        happy = document.getElementById("happy");
+        angry = document.getElementById("angry");
         window.requestAnimationFrame(loop);
     }
+    else {
+        location.reload();
+    }
 
-    // run the webcam image through the image model
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            if(prediction[i].className =="happy-human-face"){
-                happy.innerHTML = prediction[i].probability.toFixed(2);
-                document.getElementById("happy-progress").value = prediction[i].probability.toFixed(2);
-            }
-            // if(prediction[i].className =="sad-human-face"){
-            //     sad.innerHTML = prediction[i].probability.toFixed(2);
-            //     document.getElementById("sad-progress").value = prediction[i].probability.toFixed(2);
-            // }
-            if(prediction[i].className =="angry-human-face"){
-                angry.innerHTML = prediction[i].probability.toFixed(2);
-                document.getElementById("angry-progress").value = prediction[i].probability.toFixed(2);
-            }
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        if (prediction[i].className == "happy-human-face") {
+            happy.innerHTML = prediction[i].probability.toFixed(2);
+            document.getElementById("happy-progress").value = prediction[i].probability.toFixed(2);
+        }
+
+        if (prediction[i].className == "angry-human-face") {
+            angry.innerHTML = prediction[i].probability.toFixed(2);
+            document.getElementById("angry-progress").value = prediction[i].probability.toFixed(2);
         }
     }
+}
+
+function ifMobile(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        document.getElementById("warning").innerHTML = "Mobile Browser Does Not Work! Please open in a different browser.";
+       }
+}
